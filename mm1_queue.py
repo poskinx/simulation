326cd219ad2d86259cd69118ade686d650e5aadd
poskinx <alamoscar24@gmail.com>
2016-12-15 11:48:52 +0100

    minor changes

--- mm1_queue.py
+++ mm1_queue.py
@@ -3,13 +3,13 @@ import random
 from matplotlib import pyplot as plt
 from matplotlib import style
 
-###### General variables ######
+# General variables
 LAMBDA = 20         # paq/second
 MU = 50             # paq/second
 SERVICE_T = 0.02    # Average service time (exponential time)
 ARRIVAL_T = 0.05    # Average arrival time (exponential time)
 NUM_SERVERS = 1     # number of servers in the queue
-SIM_TIME = 350      # time of simulation in seconds, T:{35, 175, 350, 1750}
+SIM_TIME = 35       # time of simulation in seconds, T:{35, 175, 350, 1750}
 SIM_NUM = 20        # number of simulations
 TRANSFER_TIME = []  # array to accumulate the transfer time of each petition processed
 AVERAGE_TT = []     # array to accumulate the mean transfer time of each simulation
@@ -28,7 +28,7 @@ class Queue(object):
         self.server = simpy.Resource(env, num_servers)
 
     def service(self, petition, service_time):
-        """The serving processes. It takes a ``petition`` processes and tries
+        """The serving processes. It takes a ``petition`` process and tries
                 to serve it."""
         yield self.env.timeout(service_time)
         # print("%s: ##### My service time is %.5f." % (petition, service_time))
@@ -46,10 +46,10 @@ def petition(env, name, queue, service_time):
     with queue.server.request() as request:
         yield request
 
-        # print('%s enters the queue at %.5f.' % (name, env.now))
+        # print('%s enters the server at %.5f.' % (name, env.now))
         yield env.process(queue.service(name, service_time))
 
-        # print('%s leaves the queue at %.5f.' % (name, env.now))
+        # print('%s leaves the server at %.5f.' % (name, env.now))
         leaving_time = env.now
         transfer_time = (leaving_time - arriving_time)
         TRANSFER_TIME.append(transfer_time)
@@ -61,10 +61,6 @@ def setup(env, num_servers):
     ``arrival_time`` seconds."""
     # Create the queue
     queue = Queue(env, num_servers)
-
-    # # Generate 4 initial petitions
-    # for i in range(4):
-    #     env.process(petition(env, 'Petition %d' % i, queue))
 
     y = 0
     # Generate more petitions while the simulation is running
